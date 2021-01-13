3246c0a93f76c41a22416e90da74225e3fb92254
haruki_murasaki <haruki_murasaki@haruki-murasakinoMacBook-Pro.local>
2021-01-14 01:19:42 +0900

    Add eslint and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,12 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import { Link as ChakraLink, Text, Code, List, ListIcon, ListItem } from '@chakra-ui/react';
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons';
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { Hero } from '../components/Hero';
+import { Container } from '../components/Container';
+import { Main } from '../components/Main';
+import { DarkModeSwitch } from '../components/DarkModeSwitch';
+import { CTA } from '../components/CTA';
+import { Footer } from '../components/Footer';
 
 const Index = () => (
   <Container height="100vh">
@@ -27,12 +20,7 @@ const Index = () => (
       <List spacing={3} my={0}>
         <ListItem>
           <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
+          <ChakraLink isExternal href="https://chakra-ui.com" flexGrow={1} mr={2}>
             Chakra UI <LinkIcon />
           </ChakraLink>
         </ListItem>
@@ -51,6 +39,6 @@ const Index = () => (
     </Footer>
     <CTA />
   </Container>
-)
+);
 
-export default Index
+export default Index;
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,14 +1,14 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
 
-const fonts = { mono: `'Menlo', monospace` }
+const fonts = { mono: `'Menlo', monospace` };
 
 const breakpoints = createBreakpoints({
   sm: '40em',
   md: '52em',
   lg: '64em',
   xl: '80em',
-})
+});
 
 const theme = extendTheme({
   colors: {
@@ -16,6 +16,6 @@ const theme = extendTheme({
   },
   fonts,
   breakpoints,
-})
+});
 
-export default theme
+export default theme;
