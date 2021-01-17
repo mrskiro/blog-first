b6b49cbc27cebe6b602247368506a5e2c9198455
haruki_murasaki <haruki_murasaki@haruki-murasakinoMacBook-Pro.local>
2021-01-18 02:20:02 +0900

    Add About

--- src/components/SocialIcons/index.tsx
+++ src/components/SocialIcons/index.tsx
@@ -1,4 +1,4 @@
-import { Wrap, WrapItem, Tooltip, IconButton, Icon } from '@chakra-ui/react';
+import { Wrap, WrapItem, Tooltip, IconButton, Icon, Link } from '@chakra-ui/react';
 import {
   GitHubIcon,
   InstagramIcon,
@@ -14,27 +14,33 @@ export const SocialIcons = () => {
       label: 'Twitter',
       icon: TwitterIcon,
       color: 'twitter.500',
+      link: 'https://twitter.com/purp1eeeee',
     },
     {
       label: 'Instagram',
       icon: InstagramIcon,
+      link: 'https://www.instagram.com/_murasakigasuki/',
     },
     {
-      label: 'Github',
+      label: 'GitHub',
       icon: GitHubIcon,
+      link: 'https://github.com/Murasaki-1102',
     },
     {
       label: 'Zenn',
       icon: ZennIcon,
       color: '#3EA8FF',
+      link: 'https://zenn.dev/murasaki',
     },
     {
       label: 'Qiita',
       icon: QiitaIcon,
+      link: 'https://qiita.com/purple',
     },
     {
       label: 'Wantedly',
       icon: WantedlyIcon,
+      link: 'https://www.wantedly.com/id/haruki_murasaki1102',
     },
   ];
   return (
@@ -42,12 +48,14 @@ export const SocialIcons = () => {
       {icons.map((icon, index) => (
         <WrapItem key={index}>
           <Tooltip label={icon.label}>
-            <IconButton
-              aria-label={icon.label}
-              isRound
-              size="xs"
-              icon={<Icon as={icon.icon} w="4" h="4" color={icon.color} />}
-            />
+            <Link href={icon.link} isExternal>
+              <IconButton
+                aria-label={icon.label}
+                isRound
+                size="xs"
+                icon={<Icon as={icon.icon} w="4" h="4" color={icon.color} />}
+              />
+            </Link>
           </Tooltip>
         </WrapItem>
       ))}
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,29 @@
+import { Avatar, Box, Text, Divider } from '@chakra-ui/react';
 import { Layout } from '../components/Layout';
+import { SocialIcons } from '../components/SocialIcons';
 const About = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Box py="12">
+        <Box textAlign="center">
+          <Avatar src="/hoge.jpeg" size="2xl" />
+          <Text as="h1" mt="6" fontSize="2xl" fontWeight="bold">
+            Haruki Murasaki
+          </Text>
+          <SocialIcons />
+        </Box>
+
+        <Divider borderColor="purple.300" my="4" />
+
+        <Box textAlign="center">
+          <Text as="h2">Profile</Text>
+        </Box>
+
+        <div>skills</div>
+        <div>history</div>
+      </Box>
+    </Layout>
+  );
 };
 
 export default About;
