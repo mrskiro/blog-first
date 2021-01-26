50ed73e8fae070c1acadf608c54f012ca4c20c5d
haruki_murasaki <haruki_murasaki@haruki-murasakinoMacBook-Pro.local>
2021-01-26 18:36:06 +0900

    fix fontSize

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,6 +2,6 @@ import { Box, Text } from '@chakra-ui/react';
 
 export const Footer = () => (
   <Box textAlign="center" py="4" borderTopWidth="1px">
-    <Text>© 2021 purpleeeee</Text>
+    <Text fontSize="xs">© 2021 purpleeeee</Text>
   </Box>
 );
