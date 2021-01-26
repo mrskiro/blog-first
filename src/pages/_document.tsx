4112970c41b5409ce103572d0b30d9d6779f59d2
haruki_murasaki <haruki_murasaki@haruki-murasakinoMacBook-Pro.local>
2021-01-26 22:48:44 +0900

    Fix rename favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,15 +6,15 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
-          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
-          <link rel="shortcut icon" href="/favicon/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
           <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+          <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
